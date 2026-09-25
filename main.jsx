ded30519fdde86e45f8be59d699d811355d67a06
LIF Deploy <lifdeploy@lif.zone>
2026-09-25 19:46:43 +0300

    update

--- main.jsx
+++ main.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {lif_domain_parse} from 'lif-kernel/util';
 import {sites} from './sites.js';
+
+function lif_url(site){
+  let m = site.match(/^(.*)\.lif$/);
+  let domain = lif_domain_parse(location);
+  return location.protocol+'//'+m[1]+domain.root+location.port;
+}
 
 const App = ()=>{
   let entries = Object.entries(sites);
   return (
     <div className="grid">
       {entries.map(([key, site])=>
-        <a key={key} className="card" onClick={()=>{ window.location = 'http://'+site.url; }}>
+        <a key={key} className="card" onClick={()=>{
+          window.location = lif_url(site.url); }}>
           {site.icon
             ? <img className="card-icon" src={site.icon} />
             : <div className="card-icon placeholder">{site.title[0]}</div>

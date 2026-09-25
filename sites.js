d2936f8569c5b9f066265076d991b850d7c7d4c3
Bright Life <bright@bright.center>
2026-09-25 03:21:11 +0300

    update

---
+++ sites.js
@@ -0,0 +1,24 @@
+// the lifnet reserved hardcoded hosts: /etc/hosts
+export const sites = {
+  wallet: {
+    title: 'LIF Wallet and Miner',
+    icon: '',
+    url: 'wallet.lif',
+  },
+  explorer: {
+    title: 'LIF Blockchain Explorer',
+    icon: '',
+    url: 'explorer.lif',
+  },
+  os: {
+    title: 'OS Desktop',
+    icon: '',
+    url: 'os.lif',
+  }
+  basic: {
+    title: 'Basic LIF+React example',
+    icon: '',
+    url: 'lif-basic.lif',
+  },
+};
+

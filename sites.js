8d9255918bdf982662d77c31ceb451a99516c256
LIF Deploy <lifdeploy@lif.zone>
2026-09-26 16:59:13 +0300

    update

--- sites.js
+++ sites.js
@@ -4,27 +4,32 @@ export const sites = {
     title: 'Wallet',
     icon: './wallet.png',
     url: 'https://wallet.lif',
+    git: 'https://github.com/lif-zone/lif-wallet',
   },
   mining: {
     title: 'Miner',
     icon: './miner.png',
     url: 'https://wallet.lif/mine',
+    git: 'https://github.com/lif-zone/lif-wallet',
   },
   explorer: {
     title: 'Blockchain Explorer',
     icon: './explorer.png',
     url: 'https://explorer.lif',
+    git: 'https://github.com/lif-zone/lif-explorer',
   },
   os: {
     title: 'OS Desktop',
     icon: './os.png',
     url: 'https://os.lif',
+    git: 'https://github.com/lif-zone/lif-os',
   },
   basic: {
     title: 'React Example',
     //icon: '/.lif/git/github.com/glincker/thesvg/public/icons/react/default.svg?raw=1',
     icon: './reactjs.png',
     url: 'https://lif-basic.lif',
+    git: 'https://github.com/lif-zone/lif-basic',
   },
 };
 
